2b2d1b5af371002faadee5eb5345d68a90b6fd1b
Burhan Ahmed <burhanahmed29@gmail.com>
2025-11-11 00:54:38 +0530

    Update Celery configuration and Render deployment settings

--- backend/numerai/celery.py
+++ backend/numerai/celery.py
@@ -6,7 +6,7 @@ from celery import Celery
 from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'numerai.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'numerai.settings.production')
 
 app = Celery('numerai')
 
