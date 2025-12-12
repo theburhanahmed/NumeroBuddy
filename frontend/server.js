e1ad5a26df8d99f1f733b3a0f24910544d99c423
Burhan Ahmed <burhanahmed29@gmail.com>
2025-12-12 17:27:27 +0530

    Use wrapper approach to set keep-alive timeout for Next.js standalone server

--- frontend/server.js
+++ frontend/server.js
@@ -1,35 +1,9 @@
-// Wrapper for Next.js standalone server.js to fix keep-alive issues with nginx
-// This script loads the standalone server and patches the HTTP server settings
+// Custom Next.js server wrapper for nginx proxy compatibility
+// Sets keep-alive timeout and loads the standalone server
 
-const path = require('path');
-const { createServer } = require('http');
+// Set keep-alive timeout before loading Next.js
+process.env.KEEP_ALIVE_TIMEOUT = '65000';
 
-// Set environment
-process.env.NODE_ENV = 'production';
-process.chdir(__dirname);
-
-// Load the standalone server.js
-const standaloneServer = require('./.next/standalone/server.js');
-
-// The standalone server exports a server instance
-// We need to patch its keep-alive settings
-if (standaloneServer && standaloneServer.server) {
-  const server = standaloneServer.server;
-  
-  // Configure keep-alive for nginx compatibility
-  server.keepAliveTimeout = 65000; // 65 seconds (nginx default is 60s)
-  server.headersTimeout = 66000; // Must be > keepAliveTimeout
-  
-  console.log(`> Server keep-alive configured: ${server.keepAliveTimeout}ms`);
-} else {
-  // If the structure is different, try to patch after server starts
-  // Wait a bit for server to initialize
-  setTimeout(() => {
-    const http = require('http');
-    const servers = http.globalAgent.sockets || {};
-    console.log('> Patched keep-alive settings');
-  }, 1000);
-}
-
-// Export the server
-module.exports = standaloneServer;
+// Load the standalone server.js that Next.js generates
+// In standalone mode, server.js is at the root of the standalone build
+require('./server.js');
